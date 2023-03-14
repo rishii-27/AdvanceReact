51f38b967248b8d1bf14bd14127780b543c1582f
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-03-14 16:06:01 +0530

    task8: Adding Filter year

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [expense, setExpense] = useState(props.amount);
+  // const [expense, setExpense] = useState(props.amount);
 
   // console.log("Expense Item evaluated by React");
 
-  const clickHandler = () => {
-    setExpense('100')
-    // console.log(expense);
-  }
+  // const clickHandler = () => {
+  //   setExpense('100')
+  //   // console.log(expense);
+  // }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${expense}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Expense</button>
     </Card>
   );
 }
