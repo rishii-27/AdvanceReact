743c79dc0ca43044113520e6e84229cb57c5f806
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-03-15 14:53:24 +0530

    task10: Updating Expenses Using Map()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,7 +16,11 @@ const Expenses = (props) => {
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-      <ExpenseItem
+      {props.items.map(expense => <ExpenseItem title={expense.tile}
+        amount={expense.amount}
+        date={expense.date} />)}
+
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -35,7 +39,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 }
