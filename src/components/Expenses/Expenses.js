01c6e307e2e6d153f25d2e7d9df79a3e4cd62438
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-03-16 12:55:12 +0530

    task12: Conditional Rendering of Filters in Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,15 +14,44 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
+  let expensesContent = <p style={{ color: "white" }}>No Expense Found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense => <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date} />)
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-      {filteredExpenses.map(expense => <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date} />)}
+      {/* Filter using IF */}
+
+      {expensesContent}
+
+      {/* Filter using AND operator */}
+
+      {/* {filteredExpenses.length === 0 && <p style={{ color: "white" }}>No Expense Found</p>}
+      {filteredExpenses.length > 0 && (
+        filteredExpenses.map(expense => <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date} />)
+      )} */}
+
+      {/* Filter using ternary operator */}
+
+      {/* {filteredExpenses.length === 0 ? (<p style={{ color: "white" }}>No Expense Found</p>) : (
+        filteredExpenses.map(expense => <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date} />)
+      )} */}
 
       {/* {props.items.map(expense => <ExpenseItem
         key={expense.id}
@@ -30,26 +59,6 @@ const Expenses = (props) => {
         amount={expense.amount}
         date={expense.date} />)} */}
 
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
     </Card>
   );
 }
